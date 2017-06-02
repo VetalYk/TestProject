a5327fb7c74f7de3cd8e5fbcfb57a23f9448ba11
Admin <Admin>
2017-06-02 17:31:46 +0300

    first commit, base structure site

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,39 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { ArticlesComponent } from './articles/articles.component';
+import { MoviesComponent } from './movies/movies.component';
+
+
+import {  RouterModule, Routes} from '@angular/router';
+import { LeftSidebarComponent } from './left-sidebar/left-sidebar.component';
+
+const appRoutes: Routes = [
+    { path: 'home', component: HomeComponent },
+    { path: 'articles', component: ArticlesComponent  },
+    { path: 'movies', component: MoviesComponent  }
+];
+
 
 @NgModule({
-  declarations: [
-    AppComponent
+    declarations: [
+        AppComponent,
+        NavbarComponent,
+        HomeComponent,
+        ArticlesComponent,
+        MoviesComponent,
+        LeftSidebarComponent
   ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+    providers: [],
+    bootstrap: [AppComponent]
 })
+
 export class AppModule { }
